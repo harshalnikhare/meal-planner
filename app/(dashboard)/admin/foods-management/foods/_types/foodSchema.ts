966414e5e9548f12dd962a729a61aa2e0fe500cc
harshalnikhare <harshaln369@gmail.com>
2026-02-01 19:44:25 +0530

    add serving units and create zod schemas

---
+++ app/(dashboard)/admin/foods-management/foods/_types/foodSchema.ts
@@ -0,0 +1,38 @@
+import { patterns } from "@/lib/constants";
+import { regexSchema, requiredStringSchema } from "@/lib/zodSchemas";
+import { z } from "zod";
+
+const foodSchema = z.intersection(
+  z.object({
+    name: requiredStringSchema,
+    calories: regexSchema(patterns.zeroTo9999),
+    protein: regexSchema(patterns.zeroTo9999),
+    fat: regexSchema(patterns.zeroTo9999),
+    carbohyrates: regexSchema(patterns.zeroTo9999),
+    fiber: regexSchema(patterns.zeroTo9999),
+    sugar: regexSchema(patterns.zeroTo9999),
+    categoryId: requiredStringSchema,
+    foodServingUnits: z.array(z.object({})),
+  }),
+  z.discriminatedUnion("action", [
+    z.object({ action: z.literal("create") }),
+    z.object({ action: z.literal("update") }),
+  ]),
+);
+
+type FoodSchema = z.infer<typeof foodSchema>;
+
+const foodDefaultValues: FoodSchema = {
+  action: "create",
+  foodServingUnits: [],
+  name: "",
+  categoryId: "",
+  calories: "",
+  carbohyrates: "",
+  fat: "",
+  fiber: "",
+  protein: "",
+  sugar: "",
+};
+
+export { foodSchema, foodDefaultValues, type FoodSchema };
